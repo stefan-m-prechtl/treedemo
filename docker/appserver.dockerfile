592527316372453014f87288f4c6fc50583e4b1c
Stefan M Prechtl <stefanmprechtl@web.de>
2022-01-18 13:34:51 +0100

    Umstellung auf MariaDB

--- docker/appserver.dockerfile
+++ docker/appserver.dockerfile
@@ -5,9 +5,9 @@ FROM payara/server-full:5.2021.6-jdk11
 ENV PAYARA_ARGS --debug
 
 # MYSQL-JDBC-Driver 
-COPY mysql-connector-java-8.0.22.jar /opt/payara/appserver/glassfish/domains/domain1/lib
+COPY mariadb-java-client-2.7.2.jar /opt/payara/appserver/glassfish/domains/domain1/lib
 COPY guava-28.0-jre.jar /opt/payara/appserver/glassfish/domains/domain1/lib
- 
+
 # ASADMIN Commands to create JDBC-Resource
 COPY --chown=payara:payara ./post-boot-commands.asadmin /opt/payara/config/
 
--- docker/sqlinit/initdatabase.sql
+++ docker/sqlinit/initdatabase.sql
@@ -1,46 +1,53 @@
 -- Treedemo DB
-CREATE DATABASE IF NOT EXISTS treedb;
-USE treedb;
+CREATE DATABASE IF NOT EXISTS treedb
+  CHARACTER SET 'utf8' 
+  COLLATE 'utf8_general_ci';
 
-CREATE TABLE IF NOT EXISTS t_tree (
+-- Sequence als ID-Generator
+--CREATE SEQUENCE treedb.seq_tree_id;
+
+CREATE TABLE IF NOT EXISTS treedb.t_tree (
   id int(11) NOT NULL AUTO_INCREMENT,
   treename varchar(50) NOT NULL,
   PRIMARY KEY (id)
-) ENGINE=InnoDB AUTO_INCREMENT=36 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) CHARACTER SET 'utf8' COLLATE 'utf8_general_ci';
 
-CREATE TABLE IF NOT EXISTS t_node (
+
+CREATE TABLE IF NOT EXISTS treedb.t_node (
   id int(11) NOT NULL,
   tree_id int(11) NOT NULL,
   nodename varchar(50) NOT NULL,
-  PRIMARY KEY (id,tree_id),
-  KEY idxnode_tree_id (tree_id),
-  CONSTRAINT fknode_tree_id FOREIGN KEY (tree_id) REFERENCES t_tree (id)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  PRIMARY KEY (id, tree_id),
+  KEY idxnode_tree_id(tree_id),
+  CONSTRAINT fk_tree foreign key(tree_id) REFERENCES treedb.t_tree(id)
+) CHARACTER SET 'utf8' COLLATE 'utf8_general_ci';
 
-CREATE TABLE IF NOT EXISTS t_relation (
+
+CREATE TABLE IF NOT EXISTS treedb.t_relation (
   parent_id int(11) NOT NULL,
   child_id int(11) NOT NULL,
   tree_id int(11) NOT NULL,
   treelevel int(11) NOT NULL,
   PRIMARY KEY (tree_id,parent_id,child_id),
   KEY idxrelation_tree_id (tree_id),
-  CONSTRAINT fkrelation_tree_id FOREIGN KEY (tree_id) REFERENCES t_tree (id)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  CONSTRAINT fkrelation_tree_id FOREIGN KEY (tree_id) REFERENCES treedb.t_tree (id)
+) CHARACTER SET 'utf8' COLLATE 'utf8_general_ci';
 
 
-CREATE VIEW IF NOT EXISTS v_tree_cntNodes AS 
+CREATE VIEW IF NOT EXISTS treedb.v_tree_cntNodes AS 
 	select t.id AS id,t.treename AS name,count(n.id) AS cntnodes 
 	from (t_tree t join t_node n) 
 	where (t.id = n.tree_id) 
 	group by t.id,t.treename;
 	
-CREATE VIEW IF NOT EXISTS v_tree_maxlevel AS 
+
+CREATE VIEW IF NOT EXISTS treedb.v_tree_maxlevel AS 
 	select t.id AS id,t.treename AS name,max(r.treelevel) AS maxlevel 
 	from (t_tree t join t_relation r) 
 	where (t.id = r.tree_id) 
 	group by t.id,t.treename;	
 	
-CREATE VIEW IF NOT EXISTS v_tree_info AS 
+CREATE VIEW IF NOT EXISTS treedb.v_tree_info AS 
 	select vn.id AS id,vn.name AS name,vn.cntnodes AS cntNodes,vl.maxlevel AS maxLevel 
 	from (v_tree_cntNodes vn join v_tree_maxlevel vl) 
 	where (vn.id = vl.id);	
